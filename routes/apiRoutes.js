6bedeaa83de8aafa57fad712b4050b4cb707245d
you know who <chorvickprocode@gmail.com>
2021-03-09 20:44:34 -0500

    working on getting POST going -partially complete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const db = __dirname + "/../db/db.json"
+const db2 = require("../db/db.json");
 const { v4: uuidv4 } = require('uuid');
 uuidv4();
 ///You'll need to find a way to give each note a unique id when it's saved (look into `npm` packages that could do this for you).
@@ -13,14 +14,28 @@ module.exports = (app) => {
         // Read the db.json file
         fs.readFile(db, "utf-8", (err, data) => {
             if (err) throw err;
-            res.json(JSON.parse(data))
+            res.json(JSON.parse(data)) // Respond with the contents of the file.
+
         })
-        // Resond with the contents of the file.
+
     });
+    /// post request
+    // app.post("/api/notes", (req, res) => {
+    //     const new_note = req.body;
+    //     new_note.id = uuidv4();
+    //     db2.push(new_note);
+    //     console.log(db2);
+
+    //     fs.writeFile(path.join(__dirname, "../db/db.json"), JSONstringify(db2), (err) => {
+    //         if (err)
+    //             throw err;
+    //         res.json("response");
+    //     });
 
     app.post("/api/notes", (req, res) => {
-        let new_note = req.body;
-        new_note.id = uuidv4()
+        console.log('route hit')
+        res.json({ "value": "thinger" })
+
         // Get the payload of the uncoming note
         // Read the current db
         // Add the payload with a UUID to the parsed DB 
