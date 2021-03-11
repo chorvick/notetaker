fa37048641edcca3ffde1f0f66ead4b1e1a0652b
you know who <chorvickprocode@gmail.com>
2021-03-10 22:54:26 -0500

    data now saves/shows up with unique id number git add -A!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ const path = require("path");
 const db = __dirname + "/../db/db.json"
 const db2 = require("../db/db.json");
 const { v4: uuidv4 } = require('uuid');
-uuidv4();
+// uuidv4();
 ///You'll need to find a way to give each note a unique id when it's saved (look into `npm` packages that could do this for you).
 /////  package uuid ---NEED TO INSTALL AND LEARN HOW TO WORK ONCE BASIC WEB PAGE FUNCTIONS
 
@@ -21,27 +21,23 @@ module.exports = (app) => {
     });
     /// post request
     app.post("/api/notes", (req, res) => {
-        const new_note = req.body;
-        //new_note.id = uuidv4();
-        db2.push(new_note);
+        const newNote = req.body;
+        req.body.id = uuidv4();
+        db2.push(newNote);
         console.log(db2);
-        console.log(saveNote);
+
         fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(db2), (err) => {
-            if (err)
-                throw err;
+            if (err) throw err;
             res.json("response");
         });
 
 
 
-        // Get the payload of the uncoming note
-        // Read the current db
-        // Add the payload with a UUID to the parsed DB 
-        // Respond with the whole Array
+
     });
 
 
-    // A post route to add data to the db.json
+
 
 
     // A Delete route to remoce the note given the UUID
